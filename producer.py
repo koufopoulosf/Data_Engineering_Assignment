ad849d8db864bd1ad4249d56b04157527539548a
Filippos <66564413+koufopoulosf@users.noreply.github.com>
2023-10-04 01:16:42 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from confluent_kafka import Producer
-import csv, fastavro, io, os, sys, time, json
-from dotenv import load_dotenv
+import csv, fastavro, io, sys
+from fastapi import FastAPI, UploadFile, File, HTTPException, Query
+from fastapi.responses import ORJSONResponse
+from clickhouse_driver.errors import Error as ClickHouseError
+from confluent_kafka.error import KafkaError
+from typing import Optional
 from loguru import logger
+from config import (
+    KAFKA_TOPIC,
+    BATCH_MAX_RECORDS,
+    AVRO_SCHEMA_CONFIG,
+    initialize_kafka_producer,
+    initialize_clickhouse_client
+)
 
 # Initialize the FastAPI application instance
 app = FastAPI()
@@ -11,56 +20,8 @@ app = FastAPI()
 logger.remove()
 logger.add(sys.stdout, colorize=True, format='<green>{level}</green> | {time:DD-MM-YYYY HH:mm:ss} | <level>{message}</level>')
 
-# Load environment variables from .env file
-load_dotenv()
-batch_max_records = int(os.getenv("BATCH_MAX_RECORDS")) # Global since it will be re-used frequently later
-kafka_topic = os.getenv("KAFKA_TOPIC") # Global since it will be re-used frequently later
-
-# Read and parse the AVRO schema from the file
-schema_file_path = os.path.join("schemas", "avro_schema.json")
-with open(schema_file_path, "r") as schema_file:
-    avro_schema = json.load(schema_file)
-
-# Initialize Kafka producer
-def initialize_kafka_producer():
-    producer_config = {
-        "bootstrap.servers": os.getenv("KAFKA_BROKERS"),
-        "client.id": os.getenv("KAFKA_CLIENT_ID"),
-        "queue.buffering.max.kbytes": int(os.getenv("QUEUE_BUFFERING_MAX_KBYTES")),
-        "batch.num.messages": batch_max_records
-    }
-    try:
-        producer = Producer(producer_config)
-        return producer
-    except Exception as e:
-        logger.error(f"Error initializing Kafka producer: {str(e)}")
-        sys.exit(1)
-
-producer = initialize_kafka_producer()
-
-# Function to send records to Kafka
-def send_records_to_kafka(records):
-    try:
-        avro_bytes_list = []
-        for avro_record in records:
-            # print(avro_record)
-            # Serialize Avro records to Avro binary format
-            avro_bytes_io = io.BytesIO()
-            fastavro.schemaless_writer(avro_bytes_io, avro_schema, avro_record)
-            avro_bytes = avro_bytes_io.getvalue()
-            avro_bytes_list.append(avro_bytes)
-
-        # Send the batched Avro records to Kafka
-        for avro_bytes in avro_bytes_list:
-            producer.produce(kafka_topic, value=avro_bytes)
-        
-        producer.flush()  # Flush Kafka messages here
-
-    except Exception as e:
-        logger.error(f"Error sending records to Kafka: {str(e)}")
-
 # Function to process a CSV row and convert it to Avro record
-def process_csv_row_to_avro(row):
+def process_csv_row_to_avro(row, avro_schema):
     avro_record = {}
     for i, field in enumerate(avro_schema["fields"]):
         cell_value = row[i].strip() if i < len(row) else None
@@ -70,13 +31,36 @@ def process_csv_row_to_avro(row):
             elif field["type"] == "int":
                 avro_record[field["name"]] = int(cell_value) if cell_value else None
             elif field["type"] == "string":
-                avro_record[field["name"]] = str(cell_value)
-        except (ValueError, TypeError):
+                avro_record[field["name"]] = str(cell_value) if cell_value else None
+        except (ValueError, TypeError) as e:
             avro_record[field["name"]] = None  # Handle invalid or None values
+            logger.error(f"Error processing CSV row: {str(e)}")
     return avro_record
 
+# Function to send records to Kafka
+def send_records_to_kafka(records, kafka_producer, avro_schema):
+    try:
+        avro_bytes_list = []
+        for avro_record in records:
+            # print(avro_record) # For debug purposes
+            # Serialize Avro records to Avro binary format
+            avro_bytes_io = io.BytesIO()
+            fastavro.schemaless_writer(avro_bytes_io, avro_schema, avro_record)
+            avro_bytes = avro_bytes_io.getvalue()
+            avro_bytes_list.append(avro_bytes)
+
+        # Send the batched Avro records to Kafka
+        for avro_bytes in avro_bytes_list:
+            kafka_producer.produce(KAFKA_TOPIC, value=avro_bytes)
+        
+        kafka_producer.flush()  # Flush Kafka messages here
+
+    except Exception as e:
+        logger.error(f"Error sending records to Kafka: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error sending records to Kafka: {str(e)}")
+
 # Function to process an uploaded CSV file and publish Avro records
-async def process_uploaded_csv_to_avro(file):
+async def process_uploaded_csv_to_avro(file, kafka_producer, avro_schema):
     try:
         if not file.filename.endswith(".csv"):
             raise HTTPException(status_code=400, detail="File type is not allowed")
@@ -98,39 +82,134 @@ async def process_uploaded_csv_to_avro(file):
             if not row:
                 continue  # Skip empty rows
 
-            avro_record = process_csv_row_to_avro(row)
+            avro_record = process_csv_row_to_avro(row, avro_schema)
             avro_records_buffer.append(avro_record)
 
             # Check if the buffer size exceeds the batch size
-            if len(avro_records_buffer) >= batch_max_records:
-                send_records_to_kafka(avro_records_buffer)
+            if len(avro_records_buffer) >= BATCH_MAX_RECORDS:
+                send_records_to_kafka(avro_records_buffer, kafka_producer, avro_schema)
                 avro_records_buffer.clear()
 
         # Send any remaining records to Kafka
         if avro_records_buffer:
-            send_records_to_kafka(avro_records_buffer)
+            send_records_to_kafka(avro_records_buffer, kafka_producer, avro_schema)
 
         return {"message": "CSV data uploaded, processed, converted to Avro, and published to Kafka."}
 
-    except (HTTPException, Exception) as err:
-        logger.error(f"An error occurred: {str(err)}")
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(err)}")
+    except Exception as e:
+        logger.error(f"An error occurred during processing the uploaded CSV: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"An error occurred during processing the uploaded CSV: {str(e)}")
 
-@app.post("/upload_and_process_to_avro")
-async def upload_and_process_csv_to_avro(file: UploadFile = File(...)):
+@app.post("/csv_upload")
+async def csv_upload(csv_file: UploadFile = File(...)):
     try:
-        start_time = time.perf_counter()
-        result = await process_uploaded_csv_to_avro(file)
-        end_time = time.perf_counter()
-        processing_time = end_time - start_time
-        logger.info(f"Processing completed in {processing_time} seconds.")
-        return result
-    except HTTPException as http_err:
-        return http_err
-    except Exception as err:
-        logger.error(f"An error occurred: {str(err)}")
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(err)}")
+        kafka_producer = initialize_kafka_producer()
+        return await process_uploaded_csv_to_avro(file=csv_file, kafka_producer=kafka_producer, avro_schema=AVRO_SCHEMA_CONFIG)
+    except KafkaError as kafka_error:
+        logger.error(f"[POST Endpoint] Kafka Error: {str(kafka_error)}")
+        raise HTTPException(status_code=500, detail="Kafka encountered an error. Please check your topic and Kafka configuration.")
+    except Exception as e:
+        logger.error(f"[POST Endpoint] An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"[POST Endpoint] An error occurred: {str(e)}")
 
+@app.get("/aggregated_game_rounds")
+async def get_aggregated_game_rounds(
+    player_id: Optional[str] = Query(None, description="Player ID"),
+    game_id: Optional[int] = Query(None, description="Game ID"),
+    start_date: Optional[str] = Query(None, description="Start date in YYYY-MM-DD HH:mm:ss format"),
+    end_date: Optional[str] = Query(None, description="End date in YYYY-MM-DD HH:mm:ss format"),
+    page_num: int = Query(1, description="Page number"),
+    page_size: int = Query(10, description="Page size")
+):
+    try:
+        # Initialize ClickHouse client instance
+        client = initialize_clickhouse_client()
+
+        # Base query for data retrieval
+        data_query = """
+            SELECT
+                created_hour,
+                game_id,
+                user_id,
+                total_real_amount_bet,
+                total_bonus_amount_bet,
+                total_real_amount_win,
+                total_bonus_amount_win,
+                game_name,
+                provider
+            FROM
+                hourly_aggregated_game_rounds
+        """
+        # Base query for counting records
+        count_query = "SELECT COUNT(*) FROM hourly_aggregated_game_rounds"
+
+        # Add WHERE clause if any filter is provided
+        conditions = []
+        if player_id:
+            conditions.append(f"user_id = '{player_id}'")
+        if game_id:
+            conditions.append(f"game_id = {game_id}")
+        if start_date:
+            conditions.append(f"created_hour >= '{start_date}'")
+        if end_date:
+            conditions.append(f"created_hour < '{end_date}'")
+
+        if conditions:
+            data_query += " WHERE " + " AND ".join(conditions)
+            count_query += " WHERE " + " AND ".join(conditions)
+        
+        # Add pagination
+        data_query += f" ORDER BY created_hour LIMIT {page_size} OFFSET {(page_num - 1) * page_size};"
+
+        # Execute the data query against ClickHouse
+        results = client.execute(data_query)
+
+        # Execute the count query to get the total results count
+        total_results = client.execute(count_query)[0][0]
+  
+        pagination = {}
+        if (page_num - 1) * page_size + page_size >= total_results:
+            pagination["next"] = None
+            if page_num > 1:
+                pagination["previous"] = f"/aggregated_game_rounds?page_num={page_num-1}&page_size={page_size}"
+            else:
+                pagination["previous"] = None
+        else:
+            pagination["next"] = f"/aggregated_game_rounds?page_num={page_num+1}&page_size={page_size}"
+            if page_num > 1:
+                pagination["previous"] = f"/aggregated_game_rounds?page_num={page_num-1}&page_size={page_size}"
+            else:
+                pagination["previous"] = None
+
+        response_data = {
+            "data": [
+                {
+                    "created_hour": row[0],
+                    "game_id": row[1],
+                    "user_id": row[2],
+                    "total_real_amount_bet": row[3],
+                    "total_bonus_amount_bet": row[4],
+                    "total_real_amount_win": row[5],
+                    "total_bonus_amount_win": row[6],
+                    "game_name": row[7],
+                    "provider": row[8]
+                }
+                for row in results
+            ],
+            "total_results": total_results,
+            "pagination": pagination
+        }
+
+        return ORJSONResponse(content=response_data)
+
+    except ClickHouseError as clickhouse_error:
+        logger.error(f"[GET Endpoint] ClickHouse Error: {str(clickhouse_error)}")
+        raise HTTPException(status_code=500, detail="ClickHouse encountered an error. Please check your query and ClickHouse configuration.")
+
+    except Exception as e:
+        logger.error(f"[GET Endpoint] An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"[GET Endpoint] An error occurred: {str(e)}")
+        
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
